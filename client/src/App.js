8a3df263f9ff69b84198902c1dd9da2bd57d9a11
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-03-02 11:36:45 +0100

    git submodule bug fix

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { useEffect, useState, useRef } from 'react';
+
+function App() {
+  return (
+    <div className="game-container">
+      <div className="half-container"></div>
+      <div className="half-container"></div>
+    </div>
+  );
+}
+
+export default App;
