399a7d4abdda067e6df7b14a2a90f3d3c3176e8e
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-03-02 13:40:31 +0100

    country api data received and implemented in html

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,24 @@ import React from 'react';
 import { useEffect, useState, useRef } from 'react';
 
 function App() {
+
+  const [countriesData, setCountriesData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then(response => response.json())
+      .then(data => setCountriesData(data))  
+  }, [])
+
+  const showCountries = () => {
+    console.log(countriesData[0].flag);
+  }
+
   return (
     <div className="game-container">
     <div className="half-container">
-    <div className="flag-container-left">
-
+    <div className="flag-container-left" onClick={showCountries}>
+    <img src={countriesData[0].flags.png}></img>
     </div>
     </div>
     <div className="half-container">
