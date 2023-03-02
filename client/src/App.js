d15f79cdd9cd183bf25c80e3b043de232f76de9e
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-03-02 12:14:30 +0100

    html elements added with layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { useEffect, useState, useRef } from 'react';
@@ -6,8 +5,16 @@ import { useEffect, useState, useRef } from 'react';
 function App() {
   return (
     <div className="game-container">
-      <div className="half-container"></div>
-      <div className="half-container"></div>
+    <div className="half-container">
+    <div className="flag-container-left">
+
+    </div>
+    </div>
+    <div className="half-container">
+    <div className="flag-container-right">
+
+    </div>
+    </div>
     </div>
   );
 }
