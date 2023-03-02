12dab78eb95471fc82a4571e02543f39c0588e4a
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-03-02 18:15:22 +0100

    population layout updated and colors made conditional

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,8 @@ function App() {
   const [featuredCountryTwo, setFeaturedCountryTwo] = useState(0);
   const [gameover, setGameover] = useState(false);
   const [streak, setStreak] = useState(0);
+  const [showPopulation, setShowPopulation] = useState(false);
+  const [correct, setCorrect] = useState(false);
 
   useEffect(() => {
     setFeaturedCountryOne(Math.floor(Math.random() * Math.floor(250)));
@@ -23,27 +25,35 @@ function App() {
   }
 
   const handleFeaturedCountryOne = () => {
-    if (countriesData[featuredCountryTwo].population > countriesData[featuredCountryOne].population) {
-      setGameover(true);
-    }
-    setStreak(streak + 1);
-    if (Math.random() < 0.5) {
-      setFeaturedCountryOne(Math.floor(Math.random() * Math.floor(250)));
-    } else {
-      setFeaturedCountryTwo(Math.floor(Math.random() * Math.floor(250)));
-    }
+    setShowPopulation(true);
+    setTimeout(() => {
+      if (countriesData[featuredCountryTwo].population > countriesData[featuredCountryOne].population) {
+        setGameover(true);
+      }
+      setStreak(streak + 1);
+      setShowPopulation(false);
+      if (Math.random() < 0.5) {
+        setFeaturedCountryOne(Math.floor(Math.random() * Math.floor(250)));
+      } else {
+        setFeaturedCountryTwo(Math.floor(Math.random() * Math.floor(250)));
+      }
+    }, 300)
   }
 
   const handleFeaturedCountryTwo = () => {
-    if (countriesData[featuredCountryOne].population > countriesData[featuredCountryTwo].population) {
-      setGameover(true);
-    }
-    setStreak(streak + 1);
-    if (Math.random() < 0.5) {
-      setFeaturedCountryOne(Math.floor(Math.random() * Math.floor(250)));
-    } else {
-      setFeaturedCountryTwo(Math.floor(Math.random() * Math.floor(250)));
-    }
+    setShowPopulation(true);
+    setTimeout(() => {  
+      if (countriesData[featuredCountryOne].population > countriesData[featuredCountryTwo].population) {
+        setGameover(true);
+      }
+      setStreak(streak + 1);
+      setShowPopulation(false);
+      if (Math.random() < 0.5) {
+        setFeaturedCountryOne(Math.floor(Math.random() * Math.floor(250)));
+      } else {
+        setFeaturedCountryTwo(Math.floor(Math.random() * Math.floor(250)));
+      }
+    }, 2000)   
   }
 
   if (gameover) {
@@ -58,19 +68,19 @@ function App() {
     <div className="info-container">
     <h1 className="country-name-left">{countriesData[featuredCountryOne].name.common}</h1>
     <div className="capital-city-left">Capital: {countriesData[featuredCountryOne].capital}</div>
-    <div className="population">{countriesData[featuredCountryOne].population}</div>
+    <div className="population-left" style={{ color: showPopulation ? correct ? "white" : "red" : "transparent" }}>{countriesData[featuredCountryOne].population}</div>
     <div className="land-area">{countriesData[featuredCountryOne].area} km²</div>
     <div className="flag-container-left" onClick={showCountries}>
     <img src={countriesData[featuredCountryOne].flags.png} className="flag"></img>
     </div>
     </div>
     </div>
-    <div className="streak">{streak}</div>
+    <div className="streak">Score: {streak}</div>
     <div className="half-container" onClick={handleFeaturedCountryTwo}>
     <div className="info-container">
     <h1 className="country-name-right">{countriesData[featuredCountryTwo].name.common}</h1>
     <div className="capital-city-right">Capital: {countriesData[featuredCountryTwo].capital}</div>
-    <div className="population">{countriesData[featuredCountryTwo].population}</div>
+    <div className="population-right" style={{ color: showPopulation ? correct ? "white" : "red" : "transparent" }}>{countriesData[featuredCountryTwo].population}</div>
     <div className="land-area">{countriesData[featuredCountryTwo].area} km²</div>
     <div className="flag-container-right">
     <img src={countriesData[featuredCountryTwo].flags.png} className="flag"></img>  
